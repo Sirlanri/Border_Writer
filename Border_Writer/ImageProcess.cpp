2028d8dcba5b50a52b7d45538561e293aa603795
Rico-Station <mail@ri-co.cn>
2023-11-21 16:18:10 +0800

    新增 png图像

--- Border_Writer/ImageProcess.cpp
+++ Border_Writer/ImageProcess.cpp
@@ -32,5 +32,21 @@ bool ReadImgs(vector<string>& ImgNames)
         //_findclose函数结束查找
         _findclose(hFile);
     }
+    if ((hFile = _findfirst(p.assign(ImgPath).append("*.png").c_str(), &fileinfo)) != -1)//若查找成功，则进入
+    {
+        do
+        {
+            ImgNames.push_back(fileinfo.name);
+        } while (_findnext(hFile, &fileinfo) == 0);
+        //_findclose函数结束查找
+        _findclose(hFile);
+    }
+
+    //如果该目录没有图像，报错
+    if (!ImgNames.size())
+    {
+        llog::error("当前路径图像为空: " + ImgPath);
+        return false;
+    }
     return true;
 }
