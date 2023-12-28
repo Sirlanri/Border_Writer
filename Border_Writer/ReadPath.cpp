10b8a41ee6c2ef576861f06b2d0361cf338dc393
Rico-Station <mail@ri-co.cn>
2023-12-28 17:12:36 +0800

    修改 细边界的数量，可以读取任意数量的细边界

--- Border_Writer/ReadPath.cpp
+++ Border_Writer/ReadPath.cpp
@@ -2,39 +2,41 @@
 
 int ReadFineBorder(string imgName, vector<vector<int>>& borders)
 {
+    //所有边界的路径
+    vector<filesystem::path> paths;
 	//细边界
-	string b1 = ImgPath + R"(txt_path\xifenge\1\)";
-	string b2 = ImgPath + R"(txt_path\xifenge\2\)";
-	string b3 = ImgPath + R"(txt_path\xifenge\3\)";
-	string b4 = ImgPath + R"(txt_path\xifenge\4\)";
-	string b5 = ImgPath + R"(txt_path\xifenge\5\)";
-	string b6 = ImgPath + R"(txt_path\xifenge\6\)";
-	string b7 = ImgPath + R"(txt_path\xifenge\7\)";
-	string b8 = ImgPath + R"(txt_path\xifenge\8\)";
+    string findBorderPath = ImgPath + R"(txt_path\xifenge\)";
+    //带束层细边界
+    string findTreadBorderPath = ImgPath + R"(txt_path\xifenge_tread\)";
 
-	//带束层细边界
-	string t1 = ImgPath + R"(txt_path\xifenge_tread\1\)";
-	string t2 = ImgPath + R"(txt_path\xifenge_tread\2\)";
-	string t3 = ImgPath + R"(txt_path\xifenge_tread\3\)";
-	string t4 = ImgPath + R"(txt_path\xifenge_tread\4\)";
-
-	string paths[12] = { b1,b2,b3,b4,b5,b6,b7,b8,t1,t2,t3,t4 };
+    // 获取目录下的所有边界文件夹
+    for (auto& entry : filesystem::directory_iterator(findBorderPath)) {
+        if (entry.is_directory()) {
+            paths.push_back(entry.path());
+        }
+    }
+    for (auto& entry : filesystem::directory_iterator(findTreadBorderPath)) {
+        if (entry.is_directory()) {
+            paths.push_back(entry.path());
+        }
+    }
 
 	//获取无后缀的图片文件名
 	size_t pointIndex = imgName.rfind("."); //查找后缀名所在的位置下标
 	imgName.erase(imgName.begin() + pointIndex, imgName.end());  //截取文件名
+    imgName = imgName + ".txt";
 
-    //循环 处理12个边
-    for (int i = 0; i < 12; i++)
+    //循环 处理所有边
+    for (int i = 0; i < paths.size(); i++)
     {
         //得到完整的txt路径
-        paths[i].append(imgName).append(".txt");
+        paths[i].append(imgName);
         ifstream txtFile;
         //读取txt文件，获取边界
         txtFile.open(paths[i], ios::in);
         if (txtFile.fail())
         {
-            llog::error("ReadFineBorder-该图像边界文件可能不存在" + paths[i]);
+            llog::error("ReadFineBorder-该图像边界文件可能不存在" + paths[i].string());
             return 0;
         }
         //存储临时变量
